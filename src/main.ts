4236fef9e79547a666d39623285c8af49db154bc
Lnola <nola.luka@gmail.com>
2023-10-05 21:51:42 +0200

    :sparkles: Register auth0

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
+import auth0 from '@/plugins/auth0';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(auth0);
 
 app.mount('#app');
