efafa5e18105c08d87bc26ae3c81fa0a2845425d
Lnola <nola.luka@gmail.com>
2023-10-05 22:40:36 +0200

    :construction: Add all authentication buttons to home page

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,3 +1,34 @@
-<template>Home</template>
+<template>
+  <template v-if="!isAuthenticated">
+    <button @click="handleLogin">LOGIN</button>
+    <button @click="handleSignup">SIGNUP</button>
+  </template>
+  <template v-else>
+    <button @click="handleLogout">LOGOUT</button>
+  </template>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+const handleLogin = () => {
+  loginWithRedirect({
+    appState: { target: '/profile' },
+  });
+};
+
+const handleSignup = () => {
+  loginWithRedirect({
+    appState: { target: '/profile' },
+    authorizationParams: { screen_hint: 'signup' },
+  });
+};
+
+const handleLogout = () => {
+  logout({
+    logoutParams: { returnTo: window.location.origin },
+  });
+};
+</script>
