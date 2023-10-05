d7348716d8a5259dd48895fa54f18a8b99fc581f
Lnola <nola.luka@gmail.com>
2023-10-05 22:54:41 +0200

    :sparkles: Register primevue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from '@/app.vue';
 import router from '@/router';
 import auth0 from '@/plugins/auth0';
+import primeVue from '@/plugins/primevue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(auth0);
+app.use(primeVue);
 
 app.mount('#app');
