13d5b3f7ab726ba9088697cd9e223c749ca69153
Lnola <nola.luka@gmail.com>
2023-10-05 22:40:20 +0200

    :zap: Show loading on authentication check

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <base-loading v-if="isLoading" />
+  <router-view v-else />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+import BaseLoading from '@/components/common/base-loading.vue';
+
+const { isLoading } = useAuth0();
+</script>
