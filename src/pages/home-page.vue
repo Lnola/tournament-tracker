2b8108adf47382cfc7ecf2f2efec2a5b28aa3ed8
Lnola <nola.luka@gmail.com>
2023-10-05 23:54:58 +0200

    :fire: Remove auth options from home page

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,34 +1,3 @@
-<template>
-  <template v-if="!isAuthenticated">
-    <button @click="handleLogin">LOGIN</button>
-    <button @click="handleSignup">SIGNUP</button>
-  </template>
-  <template v-else>
-    <button @click="handleLogout">LOGOUT</button>
-  </template>
-</template>
+<template>Home</template>
 
-<script lang="ts" setup>
-import { useAuth0 } from '@auth0/auth0-vue';
-
-const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-
-const handleLogin = () => {
-  loginWithRedirect({
-    appState: { target: '/management' },
-  });
-};
-
-const handleSignup = () => {
-  loginWithRedirect({
-    appState: { target: '/management' },
-    authorizationParams: { screen_hint: 'signup' },
-  });
-};
-
-const handleLogout = () => {
-  logout({
-    logoutParams: { returnTo: window.location.origin },
-  });
-};
-</script>
+<script lang="ts" setup></script>
