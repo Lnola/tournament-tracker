6fadd9317f8adcebe5cf07dccf58ae214c05d9b5
Lnola <nola.luka@gmail.com>
2023-10-06 02:41:04 +0200

    :sparkles: Create a common input number component

---
+++ src/components/common/t-input-number.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+    <label :for="inputId" class="block text-xs mb-2">{{ label }}</label>
+    <InputNumber
+      v-bind="bind"
+      showButtons
+      buttonLayout="vertical"
+      :style="{ width }"
+      :input-id="inputId"
+      :disabled="disabled"
+    />
+  </span>
+</template>
+
+<script lang="ts" setup>
+import InputNumber from 'primevue/inputnumber';
+
+defineProps({
+  bind: { type: Object, required: true },
+  label: { type: String, required: true },
+  inputId: { type: String, required: true },
+  disabled: { type: Boolean, default: false },
+  width: { type: String, default: '4rem' },
+});
+</script>
