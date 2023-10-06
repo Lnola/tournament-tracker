abbcd7eff7ee1b261b5babb5e175e369c40f0f97
Lnola <nola.luka@gmail.com>
2023-10-06 02:50:07 +0200

    :recycle: Update inputs and use vee-validate

--- src/components/Management/management-create.vue
+++ src/components/Management/management-create.vue
@@ -1,80 +1,78 @@
 <template>
-  <div class="flex flex-column align-items-center">
-    <span class="p-float-label mb-5">
-      <InputText
-        id="tournament-name"
-        v-model="tournamentName"
-        style="width: 15rem"
-        :disabled="disabled"
-      />
-      <label for="tournament-name">Tournament name</label>
-    </span>
-    <span class="p-float-label mb-3">
-      <Chips
-        input-id="team-names"
-        v-model="teamNames"
-        separator=";"
-        :pt="{ container: { style: { width: '15rem' } } }"
-        :max="8"
-        :min="4"
-        :disabled="disabled"
-      />
-      <label for="team-names">Chips</label>
-    </span>
+  <form @submit="onSubmit" class="flex flex-column align-items-center">
+    <TInputText
+      :bind="tournamentName"
+      :disabled="disabled"
+      :error-message="errors.tournamentName"
+      label="Tournament name"
+      input-id="tournament-name"
+    />
+    <TChips
+      :bind="teamNames"
+      :disabled="disabled"
+      :error-message="errors.teamNames"
+      label="Team names"
+      input-id="team-names"
+    />
 
     <section class="flex gap-3 mb-5">
-      <span>
-        <label for="points-win" class="block text-xs mb-2">Points Win</label>
-        <InputNumber
-          input-id="points-win"
-          v-model="points.win"
-          showButtons
-          buttonLayout="vertical"
-          style="width: 4rem"
-          :disabled="disabled"
-        />
-      </span>
-      <span>
-        <label for="points-draw" class="block text-xs mb-2">Points Draw</label>
-        <InputNumber
-          input-id="points-draw"
-          v-model="points.draw"
-          showButtons
-          buttonLayout="vertical"
-          style="width: 4rem"
-          :disabled="disabled"
-        />
-      </span>
-      <span>
-        <label for="points-loss" class="block text-xs mb-2">Points Loss</label>
-        <InputNumber
-          input-id="points-loss"
-          v-model="points.loss"
-          showButtons
-          buttonLayout="vertical"
-          style="width: 4rem"
-          :disabled="disabled"
-        />
-      </span>
+      <TInputNumber
+        :bind="pointsWin"
+        :disabled="disabled"
+        label="Points Win"
+        input-id="points-win"
+      />
+      <TInputNumber
+        :bind="pointsDraw"
+        :disabled="disabled"
+        label="Points Draw"
+        input-id="points-draw"
+      />
+      <TInputNumber
+        :bind="pointsLoss"
+        :disabled="disabled"
+        label="Points Loss"
+        input-id="points-loss"
+      />
     </section>
 
-    <Button type="submit" label="Submit" class="w-15rem" />
-  </div>
+    <Button type="submit" label="Create" class="w-15rem" />
+  </form>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from 'vue';
-import InputText from 'primevue/inputtext';
-import Chips from 'primevue/chips';
-import InputNumber from 'primevue/inputnumber';
+import { useForm } from 'vee-validate';
+import { object, string, array, number } from 'yup';
+import { toTypedSchema } from '@vee-validate/yup';
 import Button from 'primevue/button';
-import { type Points } from '@/types/points';
+import TInputText from '@/components/common/t-input-text.vue';
+import TInputNumber from '@/components/common/t-input-number.vue';
+import TChips from '@/components/common/t-chips.vue';
 
 defineProps({
   disabled: { type: Boolean, default: false },
 });
 
-const tournamentName = ref<string>('');
-const teamNames = ref<string[]>([]);
-const points = reactive<Points>({ win: 0, draw: 0, loss: 0 });
+const { handleSubmit, defineInputBinds, defineComponentBinds, errors } =
+  useForm({
+    validationSchema: toTypedSchema(
+      object({
+        tournamentName: string().required(),
+        teamNames: array().of(string()).min(4).max(8).required(),
+        points: object({
+          win: number().required().default(0),
+          draw: number().required().default(0),
+          loss: number().required().default(0),
+        }),
+      }),
+    ),
+  });
+
+const tournamentName = defineInputBinds('tournamentName');
+const teamNames = defineComponentBinds('teamNames');
+const pointsWin = defineComponentBinds('points.win');
+const pointsDraw = defineComponentBinds('points.draw');
+const pointsLoss = defineComponentBinds('points.loss');
+
+const onSubmit = handleSubmit((values) => console.log(values));
 </script>
