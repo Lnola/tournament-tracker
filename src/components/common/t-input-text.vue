08e1dc6f5dc2e3980aff425e76d7c39430019f93
Lnola <nola.luka@gmail.com>
2023-10-06 02:46:07 +0200

    :sparkles: Create a common text input component

---
+++ src/components/common/t-input-text.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="p-float-label mb-5">
+    <InputText
+      v-bind="bind"
+      :id="inputId"
+      :style="{ width }"
+      :disabled="disabled"
+    />
+    <label :for="inputId">{{ label }}</label>
+    <div>{{ errorMessage }}</div>
+  </span>
+</template>
+
+<script lang="ts" setup>
+import InputText from 'primevue/inputtext';
+
+defineProps({
+  bind: { type: Object, required: true },
+  label: { type: String, required: true },
+  inputId: { type: String, required: true },
+  errorMessage: { type: String, default: '' },
+  disabled: { type: Boolean, default: false },
+  width: { type: String, default: '15rem' },
+});
+</script>
