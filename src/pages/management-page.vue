8666ac57f98559c5f747e092c898e6c92afe5b50
Lnola <nola.luka@gmail.com>
2023-10-06 03:16:14 +0200

    :sparkles: Add management create to management page

--- src/pages/management-page.vue
+++ src/pages/management-page.vue
@@ -1,3 +1,15 @@
-<template>Management</template>
+<template>
+  <main class="flex flex-column align-items-center justify-content-center px-7">
+    <ManagementCreate />
+  </main>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ManagementCreate from '@/components/Management/management-create.vue';
+</script>
+
+<style scoped>
+main {
+  height: calc(100% - 72px);
+}
+</style>
