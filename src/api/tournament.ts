ad519b2b10b23832d35b232b139e9a026744b89c
Lnola <nola.luka@gmail.com>
2023-10-07 23:30:11 +0200

    :label: Use common types for the dto

--- src/api/tournament.ts
+++ src/api/tournament.ts
@@ -1,17 +1,11 @@
 import { v4 as uuid } from 'uuid';
 import db, { ref, set } from '@/plugins/firebase';
+import { Tournament } from '@/types/tournament';
 
-type CreateTournamentDto = {
-  name: string;
-  competitors: string[];
-  points: {
-    win: number;
-    draw: number;
-    loss: number;
-  };
-};
+type CreateTournamentDto = Pick<Tournament, 'name' | 'competitors' | 'points'>;
 
 export const createTournament = (dto: CreateTournamentDto) => {
   const location = ref(db, `tournament/${uuid()}`);
-  return set(location, dto);
+  const data = dto;
+  return set(location, data);
 };
