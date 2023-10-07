7e981ab12a4c76f24578564eb73bdcf05d9101c6
Lnola <nola.luka@gmail.com>
2023-10-08 00:23:51 +0200

    :truck: Move tournamentId creation to consumer

--- src/api/tournament.ts
+++ src/api/tournament.ts
@@ -1,13 +1,15 @@
-import { v4 as uuid } from 'uuid';
 import db, { ref, set } from '@/plugins/firebase';
 import { Game, Tournament } from '@/types/tournament';
 import { generateBrackets } from '@/utils/generate-brackets';
 
 type CreateTournamentDto = Pick<Tournament, 'name' | 'competitors' | 'points'>;
 
-export const createTournament = (dto: CreateTournamentDto) => {
+export const createTournament = (
+  tournamentId: string,
+  dto: CreateTournamentDto,
+) => {
   const schedule: Game[] = generateBrackets(dto.competitors);
-  const location = ref(db, `tournament/${uuid()}`);
+  const location = ref(db, `tournament/${tournamentId}`);
   const data: Tournament = { ...dto, schedule };
   return set(location, data);
 };
