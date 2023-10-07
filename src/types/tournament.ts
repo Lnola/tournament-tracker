710833854c613184fc0dfc0e6c448bc036f8d402
Lnola <nola.luka@gmail.com>
2023-10-07 23:29:38 +0200

    :label: Add some basic types

---
+++ src/types/tournament.ts
@@ -0,0 +1,21 @@
+export type Competitor = string;
+
+export type Game = {
+  home: Competitor;
+  away: Competitor;
+  homeScore: number;
+  awayScore: number;
+};
+
+export type Points = {
+  win: number;
+  draw: number;
+  loss: number;
+};
+
+export type Tournament = {
+  name: string;
+  competitors: Competitor[];
+  points: Points;
+  schedule: Game[];
+};
