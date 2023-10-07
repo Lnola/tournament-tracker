b93dffcfdda28baa9b48860968e6d6c5d709b877
Lnola <nola.luka@gmail.com>
2023-10-07 23:49:26 +0200

    :sparkles: Add the schedule to the tournament creation

--- src/api/tournament.ts
+++ src/api/tournament.ts
@@ -1,11 +1,13 @@
 import { v4 as uuid } from 'uuid';
 import db, { ref, set } from '@/plugins/firebase';
-import { Tournament } from '@/types/tournament';
+import { Game, Tournament } from '@/types/tournament';
+import { generateBrackets } from '@/utils/generate-brackets';
 
 type CreateTournamentDto = Pick<Tournament, 'name' | 'competitors' | 'points'>;
 
 export const createTournament = (dto: CreateTournamentDto) => {
+  const schedule: Game[] = generateBrackets(dto.competitors);
   const location = ref(db, `tournament/${uuid()}`);
-  const data = dto;
+  const data: Tournament = { ...dto, schedule };
   return set(location, data);
 };
