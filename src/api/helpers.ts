7ee94c789608ba4d2da8c9f544422edff09c0345
Lnola <nola.luka@gmail.com>
2023-10-09 21:43:26 +0200

    :sparkles: Create a getValue helper method for firebase queries

--- src/api/helpers.ts
+++ src/api/helpers.ts
@@ -1,6 +1,14 @@
 import { User } from '@auth0/auth0-vue';
+import { Query } from 'firebase/database';
+import { get } from '@/plugins/firebase';
 
 export const getTournamentId = (user: User) => {
   if (!user.value?.sub) return;
   return user.value.sub;
+};
+
+export const getValue = async (snapshotRef: Query) => {
+  const snapshot = await get(snapshotRef);
+  if (!snapshot.exists()) throw Error("The data doesn't exist");
+  return snapshot.val();
 };
