8d24e07d8516fb592357422456500ff2b37c4bce
Lnola <nola.luka@gmail.com>
2023-10-09 21:44:31 +0200

    :recycle: Leverage getValue for existing get api calls

--- src/api/tournament.ts
+++ src/api/tournament.ts
@@ -1,14 +1,14 @@
 import { v4 as uuid } from 'uuid';
-import db, { get, child, ref, set } from '@/plugins/firebase';
-import { Round, Tournament } from '@/types/tournament';
+import db, { query, ref, set } from '@/plugins/firebase';
+import { getValue } from './helpers';
 import { generateBrackets } from '@/utils/generate-brackets';
+import { Round, Tournament } from '@/types/tournament';
 
 type CreateTournamentDto = Pick<Tournament, 'name' | 'competitors' | 'points'>;
 
 export const getTournament = async (tournamentId: string) => {
-  const snapshot = await get(child(ref(db), `tournament/${tournamentId}`));
-  if (!snapshot.exists()) throw Error("The data doesn't exist");
-  return snapshot.val();
+  const tournamentRef = query(ref(db, `tournament/${tournamentId}`));
+  return getValue(tournamentRef);
 };
 
 export const createTournament = (
