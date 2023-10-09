557a23c7ca1f5cd6aeb703f4aac9faa4ce4065f6
Lnola <nola.luka@gmail.com>
2023-10-09 11:47:50 +0200

    :sparkles: Add results to a accordion layout card

---
+++ src/components/Management/management-results.vue
@@ -0,0 +1,34 @@
+<template>
+  <Card class="w-full">
+    <template #title>{{ tournament.name }}</template>
+    <template #content>
+      <ScrollPanel style="height: 30rem">
+        <Accordion :activeIndex="0" multiple>
+          <AccordionTab
+            v-for="(round, roundIndex) in tournament.schedule"
+            :key="round.toString()"
+            :header="getRoundTitle(roundIndex)"
+          >
+            <ManagementGames :round="round" />
+          </AccordionTab>
+        </Accordion>
+      </ScrollPanel>
+    </template>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import Card from 'primevue/card';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import ScrollPanel from 'primevue/scrollpanel';
+import ManagementGames from './management-games.vue';
+import { Tournament } from '@/types/tournament';
+
+defineProps({
+  tournament: { type: Object as PropType<Tournament>, required: true },
+});
+
+const getRoundTitle = (roundIndex: number) => `Round #${roundIndex + 1}`;
+</script>
