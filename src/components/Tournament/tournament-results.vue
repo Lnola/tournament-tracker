5334a3ab1243d1242cb71cc708487e47f00b4aa9
Lnola <nola.luka@gmail.com>
2023-10-09 23:25:32 +0200

    :sparkles: Add results to tournament results page

---
+++ src/components/Tournament/tournament-results.vue
@@ -0,0 +1,13 @@
+<template>
+  <TResults :tournament="tournament" />
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import TResults from '@/components/common/t-results.vue';
+import { Tournament } from '@/types/tournament';
+
+defineProps({
+  tournament: { type: Object as PropType<Tournament>, required: true },
+});
+</script>
--- src/pages/tournament-page.vue
+++ src/pages/tournament-page.vue
@@ -1,11 +1,22 @@
-<template>{{ props }}</template>
+<template>
+  <main class="flex flex-column align-items-center justify-content-center px-7">
+    <TournamentResults :tournament="tournament" />
+  </main>
+</template>
 
 <script lang="ts" setup>
 import { PropType } from 'vue';
+import TournamentResults from '@/components/Tournament/tournament-results.vue';
 import { Tournament } from '@/types/tournament';
 
-const props = defineProps({
+defineProps({
   publicId: { type: String, required: true },
   tournament: { type: Object as PropType<Tournament>, required: true },
 });
 </script>
+
+<style scoped>
+main {
+  height: calc(100% - 72px);
+}
+</style>
