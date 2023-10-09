e59836a30265225e0d2cf8dc35cad1e64521568f
Lnola <nola.luka@gmail.com>
2023-10-09 21:53:27 +0200

    :technologist: Add query params to getTournament

--- src/api/tournament.ts
+++ src/api/tournament.ts
@@ -1,13 +1,25 @@
 import { v4 as uuid } from 'uuid';
-import db, { query, ref, set } from '@/plugins/firebase';
+import db, { equalTo, orderByChild, query, ref, set } from '@/plugins/firebase';
 import { getValue } from './helpers';
 import { generateBrackets } from '@/utils/generate-brackets';
 import { Round, Tournament } from '@/types/tournament';
+import { QueryConstraint } from 'firebase/database';
 
+type GetTournamentOrderBy = {
+  key: 'publicId';
+  value: string;
+};
 type CreateTournamentDto = Pick<Tournament, 'name' | 'competitors' | 'points'>;
 
-export const getTournament = async (tournamentId: string) => {
-  const tournamentRef = query(ref(db, `tournament/${tournamentId}`));
+export const getTournament = async (
+  tournamentId: string,
+  orderBy: GetTournamentOrderBy | null = null,
+) => {
+  const params = [
+    orderBy && orderByChild(orderBy.key),
+    orderBy && equalTo(orderBy.value),
+  ].filter(Boolean) as QueryConstraint[];
+  const tournamentRef = query(ref(db, `tournament/${tournamentId}`), ...params);
   return getValue(tournamentRef);
 };
 
