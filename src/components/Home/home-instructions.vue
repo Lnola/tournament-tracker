6b7c5c68f949d640588b81d4e9c794f86ce26a63
Lnola <nola.luka@gmail.com>
2023-10-10 13:15:14 +0200

    :sparkles: Add manage instructions

---
+++ src/components/Home/home-instructions.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="flex flex-column gap-3 mb-5">
+    <span v-if="!isAuthenticated">Sign in to create a new tournament</span>
+    <Button
+      label="Manage a tournament"
+      icon="pi pi-plus"
+      :disabled="!isAuthenticated"
+    />
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+import Button from 'primevue/button';
+
+const { isAuthenticated } = useAuth0();
+</script>
--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -2,10 +2,12 @@
   <main
     class="app-content flex flex-column align-items-center justify-content-center px-7"
   >
+    <HomeInstructions />
     <HomeList />
   </main>
 </template>
 
 <script lang="ts" setup>
+import HomeInstructions from '@/components/Home/home-instructions.vue';
 import HomeList from '@/components/Home/home-list.vue';
 </script>
