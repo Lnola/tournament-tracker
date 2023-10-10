c3136f64c090cc45ef301fb347766ae2ac0ed971
Lnola <nola.luka@gmail.com>
2023-10-10 15:06:07 +0200

    :technologist: Leverage the disableable router link

--- src/components/Home/home-instructions.vue
+++ src/components/Home/home-instructions.vue
@@ -1,22 +1,20 @@
 <template>
   <span class="flex flex-column gap-3 mb-5">
     <span v-if="!isAuthenticated">Sign in to create a new tournament</span>
-    <component
-      :is="isAuthenticated ? 'RouterLink' : 'span'"
-      :to="{ name: 'Management' }"
-    >
+    <TRouterLink :to="{ name: 'Management' }" :disabled="!isAuthenticated">
       <Button
         label="Manage a tournament"
         icon="pi pi-plus"
         :disabled="!isAuthenticated"
       />
-    </component>
+    </TRouterLink>
   </span>
 </template>
 
 <script lang="ts" setup>
 import { useAuth0 } from '@auth0/auth0-vue';
 import Button from 'primevue/button';
+import TRouterLink from '@/components/common/t-router-link.vue';
 
 const { isAuthenticated } = useAuth0();
 </script>
