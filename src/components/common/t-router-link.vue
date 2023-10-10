46593ec9cdb1ff272e693cfdbe657eb94855b422
Lnola <nola.luka@gmail.com>
2023-10-10 15:05:46 +0200

    :sparkles: Create a disableable router link component

---
+++ src/components/common/t-router-link.vue
@@ -0,0 +1,11 @@
+<template>
+  <component :is="disabled ? 'span' : 'RouterLink'" v-bind="$attrs">
+    <slot></slot>
+  </component>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  disabled: { type: Boolean, default: false },
+});
+</script>
